67241ded3fb7c61cc2a1b7dc14619d794989e2d8
Abhijana <abhijanaramanda@gmail.com>
2024-07-06 17:16:42 +0800

    feat: handle init

--- session/io/src/lib.rs
+++ session/io/src/lib.rs
@@ -1,1 +1,32 @@
+#![no_std]
+use gmeta::{InOut, Metadata};
+use gstd::{prelude::*, ActorId};
 
+pub struct SessionMetadata;
+impl Metadata for SessionMetadata {
+    type Init = ();
+    type Handle = InOut<Action, Event>;
+    type Others = ();
+    type Reply = ();
+    type Signal = ();
+    type State = ();
+}
+
+#[derive(Debug, Clone, Encode, Decode, TypeInfo)]
+pub enum Action {
+    StartGame { user: ActorId },
+    CheckWord { user: ActorId, word: String },
+}
+
+#[derive(Debug, Clone, Encode, Decode, TypeInfo)]
+pub enum Event {
+    GameStarted {
+        user: ActorId,
+    },
+    WordChecked {
+        user: ActorId,
+        correct_positions: Vec<u8>,
+        contained_in_word: Vec<u8>,
+    },
+    MessageSent,
+}
--- session/src/lib.rs
+++ session/src/lib.rs
@@ -1,1 +1,14 @@
+#![no_std]
+use gstd::{msg, prelude::*, ActorId};
 
+static mut SESSION: Option<Session> = None;
+
+struct Session {
+    target_program_id: ActorId,
+}
+
+#[no_mangle]
+extern "C" fn init() {
+    let target_program_id = msg::load().expect("Could not decode target program ID");
+    unsafe { SESSION = Some(Session { target_program_id }) }
+}
