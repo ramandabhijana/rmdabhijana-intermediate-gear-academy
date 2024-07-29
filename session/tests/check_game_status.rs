82ed9f2ddaf48987f396c5d70232b0a3042f8d13
Abhijana <abhijanaramanda@gmail.com>
2024-07-29 16:13:21 +0800

    test: finish test for check game status

--- session/tests/check_game_status.rs
+++ session/tests/check_game_status.rs
@@ -1,8 +1,11 @@
 mod utils;
 
 use gstd::MessageId;
-use gtest::Log;
-use session::consts::{err_msgs::PROGRAM_ONLY, game_rules::DELAY_CHECK_STATUS_DURATION};
+use gtest::{Log, Program};
+use session::consts::{
+    err_msgs::PROGRAM_ONLY,
+    game_rules::{DELAY_CHECK_STATUS_DURATION, MAX_ATTEMPTS},
+};
 use session_io::{Action, Event, GameOverStatus, GameStatus, State};
 use utils::*;
 
@@ -29,10 +32,57 @@ fn check_game_status_should_fail_when_called_by_other_actor() {
 }
 
 #[test]
-fn check_game_status_ignore_when_completed() {}
+fn check_game_status_ignore_when_completed() {
+    let system = init_system();
+    let proxy_program = init_programs(&system).proxy_program;
+
+    // Given: maximum number of attempts is reached
+    proxy_program.send(USER, Action::StartGame);
+    consume_all_attempts_with_wrong_answers(&proxy_program);
+    let State { players, .. } = proxy_program.read_state(0).unwrap();
+    let info = players.get(&USER.into()).unwrap();
+    assert_eq!(
+        info.game_status,
+        GameStatus::Completed(GameOverStatus::Lose)
+    );
+    assert_eq!(info.attempts_count, MAX_ATTEMPTS);
+
+    // When: check status period has come
+    let result = system.spend_blocks(DELAY_CHECK_STATUS_DURATION);
+
+    // Then: Result logs are empty
+    assert!(result.first().unwrap().log().is_empty());
+}
 
 #[test]
-fn check_game_status_ignore_when_init_id_changed() {}
+fn check_game_status_ignore_when_init_id_changed() {
+    let system = init_system();
+    let proxy_program = init_programs(&system).proxy_program;
+
+    // Given:
+    // - Maximum number of attempts is reached
+    // - User restarts the game at block `DELAY_CHECK_STATUS_DURATION` - 1
+    proxy_program.send(USER, Action::StartGame);
+    consume_all_attempts_with_wrong_answers(&proxy_program);
+    let State { players, .. } = proxy_program.read_state(0).unwrap();
+    let info = players.get(&USER.into()).unwrap();
+    assert_eq!(info.attempts_count, MAX_ATTEMPTS);
+
+    let prev_init_id = info.init_msg_id;
+
+    system.spend_blocks(DELAY_CHECK_STATUS_DURATION - 1);
+    proxy_program.send(USER, Action::StartGame);
+    let State { players, .. } = proxy_program.read_state(0).unwrap();
+    let info = players.get(&USER.into()).unwrap();
+    assert_eq!(info.game_status, GameStatus::InProgress);
+
+    // When: check status period from previous session has come
+    let result = system.spend_blocks(1);
+
+    // Then: Result logs are empty and init ID's mismatch
+    assert!(result.first().unwrap().log().is_empty());
+    assert_ne!(prev_init_id, info.init_msg_id);
+}
 
 #[test]
 fn check_game_status_should_declare_game_over_when_time_is_up() {
@@ -60,4 +110,15 @@ fn check_game_status_should_declare_game_over_when_time_is_up() {
         info.game_status,
         GameStatus::Completed(GameOverStatus::Lose)
     );
+}
+
+fn consume_all_attempts_with_wrong_answers(program: &Program) {
+    for _ in 0..MAX_ATTEMPTS {
+        program.send(
+            USER,
+            Action::CheckWord {
+                word: "human".into(),
+            },
+        );
+    }
 }
